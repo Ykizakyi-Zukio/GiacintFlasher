6b9b75e1786e9255fba81c07e292451fd18760c2
YZukio <100427103+Ykizakyi-Zukio@users.noreply.github.com>
2025-10-29 14:42:49 +0600

    v

--- Giacint Flasher/Lib/Services/Debug.cs
+++ Giacint Flasher/Lib/Services/Debug.cs
@@ -11,32 +11,34 @@ internal static class Debug
     internal static void Error(Exception ex) => Error(ex.ToString());
     internal static void Error(string message)
     {
-        Console.Error.WriteLine($"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {Color.Error}× {message}");
+        Console.Error.WriteLine($"{CalcTime} {Color.Error}× {message}");
         Console.ForegroundColor = ConsoleColor.White;
     }
 
     internal static void Warning(string message)
     {
-        Console.WriteLine($"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {Color.Warning}⚠  {message}");
+        Console.WriteLine($"{CalcTime} {Color.Warning}⚠  {message}");
         Console.ForegroundColor = ConsoleColor.White;
     }
 
     internal static void Success(string message)
     {
-        Console.WriteLine($"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {Color.Success}✓  {message}");
+        Console.WriteLine($"{CalcTime} {Color.Success}✓  {message}");
         Console.ForegroundColor = ConsoleColor.White;
     }
 
     internal static void Info(string message)
     {
         if (message == "") return;
-        Console.WriteLine($"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {Color.Info}ⓘ  {message}");
+        Console.WriteLine($"{CalcTime} {Color.Info}ⓘ  {message}");
         Console.ForegroundColor = ConsoleColor.White;
     }
 
     internal static string? Input()
     {
-        Console.Write($"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] ~ {Environment.UserName} ->{Color.Info} ");
+        Console.Write($"{CalcTime} ~ {Environment.UserName} ->{Color.Info} ");
         return Console.ReadLine();
     }
+
+    private static string CalcTime() => $"{Color.Reset}[{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}]";
 }
